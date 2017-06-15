65845d0fa509d5e80e9a0523177efb1bb29a5d41
miraks <a@vldkn.net>
2017-06-15 12:09:50 +0300

    bump version

--- apps/btce_watcher/mix.exs
+++ apps/btce_watcher/mix.exs
@@ -4,7 +4,7 @@ defmodule BtceWatcher.Mixfile do
   def project do
     [
       app: :btce_watcher,
-      version: "0.1.1",
+      version: "0.1.2",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
