36147ffa59eb5b6d7dabc602f9e05b6fff7ff905
miraks <a@vldkn.net>
2018-05-15 16:38:26 +0300

    Bump version

--- apps/moex_helper/mix.exs
+++ apps/moex_helper/mix.exs
@@ -4,7 +4,7 @@ defmodule MoexHelper.Mixfile do
   def project do
     [
       app: :moex_helper,
-      version: "0.5.0",
+      version: "0.5.1",
       elixir: "~> 1.6",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix] ++ Mix.compilers,
--- rel/config.exs
+++ rel/config.exs
@@ -9,7 +9,7 @@ environment :prod do
 end
 
 release :echo do
-  set version: "0.5.0"
+  set version: "0.5.1"
   set applications: [
     cryptoc: :permanent,
     moex_helper: :permanent,
